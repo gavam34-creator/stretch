8eb70a37e3a10752676b15c98c81826328af0f7b
gavam34-creator <gavam34@gmail.com>
2026-09-24 20:40:35 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -1,67 +1,84 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
+#import <QuartzCore/QuartzCore.h>
 
-static CGFloat targetAspect = 4.0 / 3.0;
+static CALayer *g_target = nil;
+static BOOL g_active = NO;
 
-@interface UIScreen (Stretch)
-@end
-@implementation UIScreen (Stretch)
-- (CGRect)stretch_bounds {
-    CGRect real = [self stretch_bounds];
-    if (real.size.height <= 0 || real.size.width <= 0) return real;
-    CGFloat h = real.size.height;
-    CGFloat w = h * targetAspect;
-    return CGRectMake(0, 0, w, h);
-}
-@end
-
-// Рекурсивно ставит гравити "resize" (растянуть без сохранения пропорций) на слой и все подслои
-static void forceStretch(CALayer *layer) {
-    if (!layer) return;
-    layer.contentsGravity = @"resize";
-    layer.masksToBounds = NO;
-    NSArray *subs = layer.sublayers;
-    for (CALayer *sub in subs) {
-        forceStretch(sub);
-    }
+static CGRect fs(void) {
+    return [UIScreen mainScreen].bounds;
 }
 
-// Находим корневой слой игры и растягиваем его
-static void stretchAllWindows(void) {
-    UIApplication *app = [UIApplication sharedApplication];
-    if (!app) return;
-    for (UIScene *scene in app.connectedScenes) {
-        if (![scene isKindOfClass:[UIWindowScene class]]) continue;
-        UIWindowScene *ws = (UIWindowScene *)scene;
-        for (UIWindow *window in ws.windows) {
-            forceStretch(window.layer);
+static CALayer *findLayer(void) {
+    UIWindow *w = nil;
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *s in [UIApplication sharedApplication].connectedScenes) {
+            if ([s isKindOfClass:[UIWindowScene class]]) {
+                w = ((UIWindowScene *)s).windows.firstObject;
+                break;
+            }
         }
     }
+    if (!w) w = [UIApplication sharedApplication].windows.firstObject;
+    if (!w) return nil;
+
+    CGFloat minArea = fs().size.width * fs().size.height * 0.5f;
+    NSMutableArray *q = [NSMutableArray arrayWithObject:w.layer];
+    while (q.count) {
+        CALayer *l = q.firstObject; [q removeObjectAtIndex:0];
+        if ([NSStringFromClass(l.class) isEqualToString:@"CAMetalLayer"]) {
+            if (l.bounds.size.width * l.bounds.size.height >= minArea)
+                return l;
+        }
+        if (l.sublayers) [q addObjectsFromArray:l.sublayers];
+    }
+    return nil;
 }
+
+static void (*orig_setFrame)(CALayer *, SEL, CGRect);
+static void hook_setFrame(CALayer *self, SEL _cmd, CGRect frame) {
+    if (g_active && self == g_target) {
+        orig_setFrame(self, _cmd, fs());
+        return;
+    }
+    orig_setFrame(self, _cmd, frame);
+}
+
+@interface VNTATicker : NSObject
+@end
+@implementation VNTATicker
++ (void)start {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)),
+                   dispatch_get_main_queue(), ^{
+        CADisplayLink *dl = [CADisplayLink displayLinkWithTarget:[self new]
+                                                        selector:@selector(tick:)];
+        dl.preferredFramesPerSecond = 10;
+        [dl addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    });
+}
+- (void)tick:(CADisplayLink *)dl {
+    if (!g_target) {
+        g_target = findLayer();
+        if (g_target) g_active = YES;
+    }
+    if (!g_target) return;
+    CGRect full = fs();
+    if (!CGRectEqualToRect(g_target.frame, full)) {
+        [CATransaction begin];
+        [CATransaction setDisableActions:YES];
+        g_target.frame = full;
+        g_target.position = CGPointMake(CGRectGetMidX(full), CGRectGetMidY(full));
+        [CATransaction commit];
+    }
+}
+@end
 
 __attribute__((constructor))
-static void init_hook(void) {
-    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification
-                                                      object:nil
-                                                       queue:[NSOperationQueue mainQueue]
-                                                  usingBlock:^(NSNotification *note) {
-        // 1. Хук UIScreen.bounds → 4:3 (игра рендерит в 4:3)
-        Class cls = objc_getClass("UIScreen");
-        if (cls) {
-            Method orig = class_getInstanceMethod(cls, @selector(bounds));
-            Method repl = class_getInstanceMethod(cls, @selector(stretch_bounds));
-            if (orig && repl) method_exchangeImplementations(orig, repl);
-        }
-
-        // 2. Цикл: каждые 0.2 сек насильно ставим "resize" на все слои
-        //    Это заставляет iOS растянуть 4:3 картинку на весь экран
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
-            while (1) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    stretchAllWindows();
-                });
-                [NSThread sleepForTimeInterval:0.2];
-            }
-        });
-    }];
+static void init(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        Method m = class_getInstanceMethod([CALayer class], @selector(setFrame:));
+        orig_setFrame = (void *)method_getImplementation(m);
+        method_setImplementation(m, (IMP)hook_setFrame);
+        [VNTATicker start];
+    });
 }
