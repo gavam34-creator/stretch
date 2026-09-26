4957a4c651c60561bb4133a2e5f6905a6f93d9fd
gavam34-creator <gavam34@gmail.com>
2026-09-26 13:51:20 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -1,22 +1,21 @@
 #import <UIKit/UIKit.h>
-#import <objc/runtime.h>
 #import <mach-o/dyld.h>
-#import <substrate.h>
+#import <objc/runtime.h>
 
-// Найденный офсет (vaddr)
-#define RESIZE_VADDR 0x1032D3AC0
-#define BASE_VADDR   0x100000000
+// Dobby — встроенный hook-движок, не требует substrate
+extern void *DobbyHook(void *address, void *replace, void **origin);
+
+// Офсет из Ghidra (file-offset от базы 0x100000000)
+#define RESIZE_OFFSET 0x032D3AC0
+#define BASE_VADDR    0x100000000
 
 static CGFloat targetAspect = 4.0 / 3.0;
 static BOOL stretchEnabled = YES;
 
-// Оригинальная функция
 static void (*orig_ResizeViewport)(void *self, uint32_t sizeX, uint32_t sizeY);
 
-// Наш хук
 static void hook_ResizeViewport(void *self, uint32_t sizeX, uint32_t sizeY) {
     if (stretchEnabled && sizeY > 0) {
-        // Подменяем ширину под нужный аспект
         uint32_t newX = (uint32_t)(sizeY * targetAspect);
         return orig_ResizeViewport(self, newX, sizeY);
     }
@@ -26,10 +25,11 @@ static void hook_ResizeViewport(void *self, uint32_t sizeX, uint32_t sizeY) {
 __attribute__((constructor))
 static void init_hook(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        // Вычисляем реальный адрес с учётом ASLR
         intptr_t slide = _dyld_get_image_vmaddr_slide(0);
-        uintptr_t realAddr = (uintptr_t)(RESIZE_VADDR + slide);
+        uintptr_t realAddr = (uintptr_t)(slide + RESIZE_OFFSET + BASE_VADDR);
 
-        MSHookFunction((void *)realAddr, (void *)hook_ResizeViewport, (void **)&orig_ResizeViewport);
+        DobbyHook((void *)realAddr,
+                  (void *)hook_ResizeViewport,
+                  (void **)&orig_ResizeViewport);
     });
 }
